e1d08980d34f88acb700d0e833e1229b82ff6af7
mooham7 <44150156+mooham7@users.noreply.github.com>
2020-08-24 13:03:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-HOST = "127.0.0.1"
-USER = "root"
-PASS = ""
-DATABASE = "dbmember"
+HOST = "ec2-184-73-249-9.compute-1.amazonaws.com"
+USER = "puyxmzzslqihtk"
+PASS = "159bfbf8bed1d6d5ca3cb8cde8af10f039efa48c3b48d354ac904260c8ce3b78"
+DATABASE = "dbk1siba9lmb7c"
